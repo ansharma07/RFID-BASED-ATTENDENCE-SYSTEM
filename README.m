62ba42fd6bb178be5eb1a7f63ed72059bf23ec0a
Anshul Sharma <99501931+ansharma07@users.noreply.github.com>
2022-02-13 17:59:24 +0530

    Add files via upload

---
+++ README.m
@@ -0,0 +1,10 @@
+In recent years, there have been rise in the number of applications based on Radio Frequency 
+Identification (RFID) systems and have been successfully applied to different areas as diverse as 
+transportation, health-care, agriculture, and hospitality industry to name a few. RFID technology 
+facilitates automatic wireless identification using electronic passive and active tags with suitable 
+readers. In this project, an attempt is made to solve recurrent lecture attendance monitoring 
+problem in developing countries using RFID technology. The application of RFID to student 
+attendance monitoring as developed and deployed in this study is capable of eliminating time 
+wasted during manual collection of attendance and an opportunity for the educational
+administrators to capture face-to-face classroom statistics for allocation of appropriate attendance 
+scores and for further managerial decisions
